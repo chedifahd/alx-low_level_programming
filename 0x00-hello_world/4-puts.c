6b9f7bcda28fed2849cc9a576a6cbdf276795a13
chedifahd <lahmer.chedi@etudiant-fst.utm.tn>
2023-03-20 12:50:43 -0700

    asdfg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A Program that prints a line with puts function
+ * Return : 0 (success)
+ */
+int main(void)
+{
+puts("/"Programming is like building a mutilingual puzzle");
+return 0;
+}
